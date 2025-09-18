e828a7414afa52e71ffb6231bc87c8d68cdef14e
Elliott Schmechel <elliottschmechel@gmail.com>
2025-09-18 16:27:50 -0700

    Fixed the mismatched width of the extra lines for the HeroSubheader

--- frontend/src/components/homepage/hero.tsx
+++ frontend/src/components/homepage/hero.tsx
@@ -21,23 +21,23 @@ export default function Hero() {
             </div>
             <div //HeroSubheader 
                 className="absolute left-0 right-0 4xl:top-[70%] max-xl:top-[70%] xl:top-[75%] md:top-[75%] max-md:top-[65%] max-sm:top-[70%] flex justify-center z-10 px-4">
-                <div className="relative inline-block border-1 border-gray-300 px-6 py-3 text-white text-xl rounded-none bg-black/30 backdrop-blur-[2px]">
+                <div className="relative inline-block border-0.8 border-gray-300 px-6 py-3 text-white text-xl rounded-none bg-black/30 backdrop-blur-[2px]">
                 <span // Top & Bottom extensions aligned to border center
-                  aria-hidden="true"
-                  className="
+                    aria-hidden="true"
+                    className="
                     pointer-events-none absolute inset-0
-                    before:content-[''] before:absolute before:top-[-0.5px] before:left-[-16px] before:right-[-16px] before:h-px before:bg-gray-300
-                    after:content-['']  after:absolute  after:bottom-[-0.5px] after:left-[-16px] after:right-[-16px] after:h-px  after:bg-gray-300
-                  "
-                />
-                <span // Left & Right extensions aligned to border center
-                  aria-hidden="true"
-                  className="
+                    before:content-[''] before:absolute before:top-[-0.5px] before:left-[-24px] before:right-[-24px] before:h-[2px] before:bg-gray-300
+                    after:content-['']  after:absolute  after:bottom-[-0.5px] after:left-[-24px] after:right-[-24px] after:h-[2px]  after:bg-gray-300
+                    "
+                  />
+                  <span // Left & Right extensions aligned to border center
+                    aria-hidden="true"
+                    className="
                     pointer-events-none absolute inset-0
-                    before:content-[''] before:absolute before:left-[-0.5px] before:top-[-16px] before:bottom-[-16px] before:w-px before:bg-gray-300
-                    after:content-['']  after:absolute  after:right-[-0.5px]  after:top-[-16px]  after:bottom-[-16px]  after:w-px  after:bg-gray-300
-                  "
-                />
+                    before:content-[''] before:absolute before:left-[-0.5px] before:top-[-24px] before:bottom-[-24px] before:w-[2px] before:bg-gray-300
+                    after:content-['']  after:absolute  after:right-[-0.5px]  after:top-[-24px]  after:bottom-[-24px]  after:w-[2px]  after:bg-gray-300
+                    "
+                  />
                 <p //Subheader Text
                     className="relative z-10 text-center 4xl:text-4xl 3xl:text-2xl lg:text-2xl">
                   A student-led tech club for builders,<span className="block">coders, and creatives.</span>
