2307e5f9112e580684d72c16054c46b07424a650
Axel Velasquez <yoloax11@gmail.com>
2025-09-24 22:55:57 -0700

    added the navbar and the hero to the basic layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,13 @@
 import Hero from "@/components/homepage/hero";
-import NavBar from "@/components/navBar";
+import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     <div className="m-0 p-0 overflow-x-hidden">
+
+      <NavBar />
+
+      <Hero />
+
     </div>
   );
 }
