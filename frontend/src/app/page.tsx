be87cf00844b06240a02a0d3b839d4aab1dce4a1
Aengelo Vargas <aengelomarvin@gmail.com>
2025-09-28 20:32:02 -0700

    add gradient bg to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,16 @@
 import Hero from "@/components/homepage/hero";
-import NavBar from "@/components/NavBar";
+import NavBar from "@/components/navBar";
+import DiscoverStack, {DiscoverStackItem} from "@/components/homepage/discoverStack";
+
 export default function Home() {
   return (
-    <div className="m-0 p-0 overflow-x-hidden">
+    <div className="m-0 p-0 overflow-x-hidden bg-linear-to-b from-[#02030A] to-[#02030A]">
 
       <NavBar />
 
       <Hero />
 
+      <DiscoverStack/>
     </div>
   );
 }
